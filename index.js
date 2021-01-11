6e3d43ff3dd3f05fec94b5a1429942f0b6212819
jeongyongho <chocoba03@naver.com>
2021-01-11 10:52:16 +0900

    qerqr

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {User} = require("./models/User");
+const bodyParser = require('body-parser');
+
+//application/x--www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
+
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://chocoba05:Jeongyh0503!@cluster0.qqss5.mongodb.net/<dbname>?retryWrites=true&w=majority',{
@@ -11,6 +21,28 @@ mongoose.connect('mongodb+srv://chocoba05:Jeongyh0503!@cluster0.qqss5.mongodb.ne
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕!!!! 나야나')
 })
+
+app.post('/register', (res,res)) => {
+
+//회원 가입할때 필요한 정보들을 client 에서 가져오면
+// 그것들을 데이터 베이스에 넣어준다.
+
+
+const user = new User(req.body)
+
+user.save((err.userInfo)) ==>{
+
+if(err) return res.json({success:false,err})
+return res.status(200).json({
+  success:true
+})
+
+}
+
+}
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
